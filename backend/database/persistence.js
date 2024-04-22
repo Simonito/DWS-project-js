d4b44affe74c3027eb8ec896bcaedcead6d4b131
Šimon Ukuš <ukussimon2@gmail.com>
2024-04-22 18:51:55 +0200

    Return Promise when performing DB calls
    Signed-off-by: Šimon Ukuš <ukussimon2@gmail.com>

--- backend/database/persistence.js
+++ backend/database/persistence.js
@@ -2,46 +2,54 @@ const pool = require('./connection/db');
 const dbresource = require('../common/db_resource');
 
 const create = (query, params) => {
-    pool.query(query, params, (err, results) => {
-        if (err) {
-            return new dbresource.DatabaseError(err.message);
-        } else {
-            return new dbresource.ResourceCreated();
-        }
+    return new Promise((resolve, reject) => {
+        pool.query(query, params, (err, results) => {
+            if (err) {
+                rejecet(new dbresource.DatabaseError(err.message));
+            } else {
+                resolve(new dbresource.ResourceCreated());
+            }
+        });
     });
 };
 
-const read = (query, params) => {
-    pool.query(query, params, (err, results) => {
-        if (err) {
-            return new dbresource.DatabaseError(err.message);
-        }
-
-        if (results.rowCount == 0) {
-            return new dbresource.ResourceReadEmpty();
-        } else {
-            return new dbresource.ResourceReadData(results.rows);
-        }
+const read = async (query, params) => {
+    return new Promise((resolve, reject) => {
+        pool.query(query, params, (err, results) => {
+            if (err) {
+                reject(new dbresource.DatabaseError(err.message));
+            }
+    
+            if (results.rowCount == 0) {
+                resolve(new dbresource.ResourceReadEmpty());
+            } else {
+                resolve(new dbresource.ResourceReadData(results.rows));
+            }
+        });
     });
 };
 
 const update = (query, params) => {
-    pool.query(query, params, (err, results) => {
-        if (err) {
-            return new dbresource.DatabaseError(err.message);
-        } else {
-            return new dbresource.ResourceUpdated();
-        }
+    return new Promise((resolve, reject) => {
+        pool.query(query, params, (err, results) => {
+            if (err) {
+                reject(new dbresource.DatabaseError(err.message));
+            } else {
+                resolve(new dbresource.ResourceUpdated());
+            }
+        });
     });
 };
 
 const remove = (query, params) => {
-    pool.query(query, params, (err, results) => {
-        if (err) {
-            return new dbresource.DatabaseError(err.message);
-        } else {
-            return new dbresource.ResourceDeleted();
-        }
+    return new Promise((resolve, reject) => {
+        pool.query(query, params, (err, results) => {
+            if (err) {
+                reject(new dbresource.DatabaseError(err.message));
+            } else {
+                resolve(new dbresource.ResourceDeleted());
+            }
+        });
     });
 };
 
